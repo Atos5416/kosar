bb49a84ae5270b7d431a4550aafdfbec342133e1
Atos5416 <gyulaszatmari3@gmail.com>
2024-03-04 10:25:13 +0100

    ez man jo

--- test/script.js
+++ test/script.js
@@ -1,7 +1,12 @@
-function adPont(csapat) {
+function adPont(csapat, pontok) {
     var pontSpan = document.getElementById(csapat + 'Pont');
-    var pontok = parseInt(pontSpan.innerText);
-    pontok++;
-    pontSpan.innerText = pontok;
+    var jelenlegiPontok = parseInt(pontSpan.innerText);
+    var ujPontok = jelenlegiPontok + pontok;
+    pontSpan.innerText = ujPontok;
+  }
+  
+  function resetPontok() {
+    document.getElementById('csapat1Pont').innerText = '0';
+    document.getElementById('csapat2Pont').innerText = '0';
   }
   
